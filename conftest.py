3fd42b34eda6fe7fea536e8a1fb82817e458107a
Rishabh Shetye <rishabhshetye09@gmail.com>
2026-02-10 11:25:38 +0530

    Handled the Chrome Password Manager Issue

--- conftest.py
+++ conftest.py
@@ -2,12 +2,21 @@ import os
 from dotenv import load_dotenv
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 load_dotenv()
 
 @pytest.fixture()
 def driver():
-    driver = webdriver.Chrome()
+
+    opts = Options()
+    opts.add_experimental_option("prefs", {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+        "profile.paaaword_manager_leak_detection": False
+    })
+
+    driver = webdriver.Chrome(options=opts)
     driver.maximize_window()
 
     yield driver
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,9 @@
+import time
+
 from pages.login_page import LoginPage
 
 def test_login(driver, base_url, credentials):
     login = LoginPage(driver)
     login.load(base_url)
     login.login(credentials["username"], credentials["password"])
+    time.sleep(2)
