64e05a2d6732b6f25e61facb1802ea93566c7da6
Rishabh Shetye <rishabhshetye09@gmail.com>
2026-02-10 10:10:59 +0530

    Login Page Object Development

---
+++ pages/login_page.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
+
+class LoginPage(BasePage):
+    USERNAME = (By.ID, "username")
+    PASSWORD = (By.ID, "password")
+    LOGIN_BUTTON = (By.ID, "login-button")
+
+    def load(self) :
+        self.open("https://www.saucedemo.com/")
+
+    def login(self, username: str, password: str):
+        self.type(self.USERNAME, username)
+        self.type(self.PASSWORD, password)
+        self.click(self.LOGIN_BUTTON)
+
